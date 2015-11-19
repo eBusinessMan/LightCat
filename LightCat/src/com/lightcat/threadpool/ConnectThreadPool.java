7b59fa7c999b70ae93867ca6607b6d8ce04e94a0
eBusinessMan <1264995828@qq.com>
2015-11-19 09:39:35 +0800

    创建连接池

--- LightCat/src/com/lightcat/threadpool/ConnectThreadPool.java
+++ LightCat/src/com/lightcat/threadpool/ConnectThreadPool.java
@@ -2,12 +2,4 @@ package com.lightcat.threadpool;
 
 public class ConnectThreadPool {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
